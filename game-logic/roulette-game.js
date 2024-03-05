9e97daca3e040935406edb374258ba8f56212dcc
jramos-tech <jramos@student.neumont.edu>
2024-03-04 18:32:58 -0700

    pi

--- game-logic/roulette-game.js
+++ game-logic/roulette-game.js
@@ -1,10 +1,11 @@
-const Player = require('../game-logic/player.js');
- 
+let amount = 100;
+let betType = ['color', 'oddEven', 'highLow'];
+let betValue =['red', 'odd', 'low']
+
 class Roulette {
-    player = new Player();
     constructor() {
         this.numbers = Array.from({length: 37}, (_, i) => i);
-        this.colors = Array.from({length: 37}, (_, i) => i % 2 === 0 ? 'black' : 'red');
+        this.colors = Array.from({length: 37}, (_, i) => i % 2 == 0 ? 'black' : 'red');
         this.colors[0] = 'green';
     }
 
@@ -12,17 +13,87 @@ class Roulette {
         return Math.floor(Math.random() * this.numbers.length);
     }
 
-    bet(player, guess, amount) {
+    bet(amount, betType, betValue) {
         let result = this.spin();
-        const isWin = guess === this.colors[result] || guess == result;
-        const odds = isWin ? 35 : 0; 
-        if (isWin) {
-            player.winBet(amount, odds);
-        } else {
-            player.loseBet(amount);
+        let payout = 0;
+        
+        switch (betType) {
+            case 'color':
+                if (betValue == this.colors[result]) {
+                    payout = amount * 2;
+                } else {
+                    payout -= amount;
+                }
+                break;
+            case 'oddEven':
+                if (betValue == 'odd' && result % 2 != 0) {
+                    payout = amount * 2;
+                } else {
+                    payout -= amount;
+                }
+                break;
+            case 'highLow':
+                if (betValue == 'high' && result >= 19) {
+                    payout = amount * 2;
+                } else {
+                    payout -= amount;
+                }
+                break;
+            case 'dozen':
+                if ((betValue == '1st' && result >= 1 && result <= 12) ||
+                    (betValue == '2nd' && result >= 13 && result <= 24) ||
+                    (betValue == '3rd' && result >= 25 && result <= 36)) {
+                    payout = amount * 4;
+                } else {
+                    payout -= amount;
+                }
+                break;
+            case 'column':
+                if ((betValue == '1st' && (result % 3 == 1 || result == 0)) ||
+                    (betValue == '2nd' && (result % 3 == 2 || result == 0)) ||
+                    (betValue == '3rd' && (result % 3 == 0))) {
+                    payout = amount * 4;
+                } else {
+                    payout -= amount;
+                }
+                break;
+            case 'square':
+                if (betValue.includes(result)) {
+                    payout = amount * 10;
+                } else {
+                    payout -= amount;
+                }
+                break;
+            case 'split':
+                if (betValue.includes(result)) {
+                    payout = amount * 19;
+                } else {
+                    payout -= amount;
+                }
+                break;
+            case 'straight':
+                if (betValue == result) {
+                    payout = amount * 37;
+                } else {
+                    payout -= amount;
+                }
+                break;
         }
-        return isWin;
+        
+        return payout;
     }
-    
+    rouletteGame() {
+        let totalPayout = 0;
+        for (let i = 0; i < betType.length; i++) {
+            totalPayout += this.bet(amount, betType[i], betValue[i]);
+        }
+        return totalPayout;
+    }
 }
-module.exports = Roulette;
+
+if (typeof module != 'undefined' && module.exports) {
+    module.exports = Roulette;
+}
+
+let roulette = new Roulette();
+console.log("Total Payout:", roulette.rouletteGame());
